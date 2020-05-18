d7b51d1a6192c6101fd60c437ec10614e6ba287e
andy <andy.ukpe@outlook.com>
2020-05-18 20:54:01 +0200

    aggiunto javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+alert('crea la tua playlist');
+
+document.getElementById('playlist recente') .innerHTML = 'riprodotti di recente';
+
+document.getElementById('playlist di joe') .innerHTML = 'Musica di Joe Doe';
+
+document.getElementById('Artisti') . innerHTML = 'La musica del momento';
+
+document.writtenIn('Webapp created by Andy')
